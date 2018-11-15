56c216935c28a2bd86c912c00037b31b8d1441db
KIM Shingu <creation07@naver.com>
2018-11-15 18:32:54 +0900

    18-11-15

--- workspace/spring-mvc-2/src/main/java/com/example/demo/controller/EmployeeRestController.java
+++ workspace/spring-mvc-2/src/main/java/com/example/demo/controller/EmployeeRestController.java
@@ -11,10 +11,9 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.dto.Employee;
 import com.example.demo.dto.ServerResponse;
 
+//@CrossOrigin("*")
 @RestController
 @RequestMapping("/emps")
 public class EmployeeRestController {
